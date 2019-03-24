617b58e6172f1f63b541f79d5e8c03f6007f3add
zlgan <278408588@qq.com>
2019-03-24 09:57:54 +0800

    first commit

---
+++ WPFDemo/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace WPFDemo
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("------------------退出--------------");
+        }
+    }
+}
